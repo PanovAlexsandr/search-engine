8de6631ca46ecc1cf501799e45969e5cafb9ddab
PanovAlexsandr <aleksandr.panov2000@yandex.ru>
2021-04-10 23:33:46 +0300

    update

--- find-enige.py
+++ find-enige.py
@@ -12,11 +12,13 @@ class search_word:
     spisok_file2 = []
     spisok_file3 = []
     spisok_file4 = []
-
+    
     find_file1 = []
     find_file2 = []
     find_file3 = []
     find_file4 = []
+
+    exc = []
 
     def search(self, disk, format_files, word):
         doc = ('.doc', '.docx')
@@ -45,8 +47,7 @@ class search_word:
                                 break
                     #3 <=> adding unopened files to exceptions
                     except Exception as fail:
-                        with open('fail_file.txt', 'w') as r:
-                            r.write(str(fail)+'\n')  
+                        self.exc.append(r) 
             #4 <=> open file 
             for x in self.find_file1:
                 file_name = x                    
@@ -78,8 +79,7 @@ class search_word:
                                 break
                     #3
                     except Exception as fail:
-                        with open('fail_file.txt', 'w') as r:
-                            r.write(str(fail)+'\n')  
+                        self.exc.append(r)
             #4   
             for x in self.find_file2:
                 file_name = x
@@ -112,8 +112,7 @@ class search_word:
                                 break
                     #3                                
                     except Exception as fail:
-                        with open('fail_file.txt', 'w') as r:
-                            r.write(str(fail)+'\n') 
+                        self.exc.append(r) 
             #4
             for x in self.find_file3:
                 file_name = x
@@ -145,8 +144,7 @@ class search_word:
                         pdf_file.close()
                     #3
                     except Exception as fail:
-                        with open('fail_file.txt', 'w') as r:
-                            r.write(str(fail)+'\n')  
+                        self.exc.append(r) 
             #4
             for x in self.find_file4:
                 file_name = x
@@ -155,8 +153,3 @@ class search_word:
                 if want_open == 'y':
                     directory = file_name
                     subprocess.Popen('explorer ' + directory)
-
-
-        #deleting a file with errors
-        if os.path.exists("fail_file.txt") == True:
-            os.remove('fail_file.txt')
